0ae2ceac02bd52ec7dfa08a335430117c6b310e5
Johan Thureson <johan.thureson@gmail.com>
2022-02-28 10:45:15 +0100

    Added ZoneSelectorView

---
+++ Shared/Views/PurchaseViews/ZoneSelectorView.swift
@@ -0,0 +1,20 @@
+//
+//  ZoneSelectorView.swift
+//  TicketTest
+//
+//  Created by Johan Thureson on 2022-02-28.
+//
+
+import SwiftUI
+
+struct ZoneSelectorView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ZoneSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZoneSelectorView()
+    }
+}
