9f99662f883212909316312b8e1d31aad57dc36a
Johan Thureson <johan.thureson@gmail.com>
2022-03-09 17:34:55 +0100

    HistoricalTicketView

--- Shared/Views/SubViews/HistoricalTicketView.swift
+++ Shared/Views/SubViews/HistoricalTicketView.swift
@@ -13,26 +13,38 @@ struct HistoricalTicketView<TicketHistoryViewModel>: View where TicketHistoryVie
     
     var body: some View {
         
-        VStack(alignment: .alignItems) {
+        VStack {
             
-            HStack(spacing: 8) {
+            Spacer()
+
+            HStack {
+                Spacer().frame(width: 14)
                 viewModel.operatorImage
                     .alignmentGuide(.alignItems, computeValue: { d in
                         return d[HorizontalAlignment.leading]
                     })
+                    .frame(width: 18, height: 18)
                 Text(viewModel.ticketTypeName)
                     .font(.applicationFont(withWeight: .bold, andSize: 15))
+                Spacer()
             }
             
-            Text(viewModel.priceGroupName)
-                .font(.applicationFont(withWeight: .regular, andSize: 15))
-                .alignmentGuide(.alignItems, computeValue: { d in
-                    return d[HorizontalAlignment.leading]
-                })
+            Spacer().frame(height: 7)
+
+            
+            HStack {
+                Spacer().frame(width: 14)
+                Text(viewModel.priceGroupName)
+                    .font(.applicationFont(withWeight: .regular, andSize: 15))
+                Spacer()
+            }
+            
+            Spacer()
+
         }
-        .frame(width: 132, height: 67, alignment: .center)
+        .frame(width: 132, height: 67)
         .background(Color.green)
-        .cornerRadius(9.0)
+        .cornerRadius(9)
     }
 }
 
